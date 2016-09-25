8486e4ddcd19a59fba6473146bd7182965c02d7a
LoadingByte <loadingbyte@quartercode.com>
2016-09-26 01:00:55 +0200

    Move available shader pack patchers list to ShaderPackPatcher interface
    No one cares about proper package separation anyway ...

--- src/main/java/de/unratedfilms/moviefocus/fmlmod/hooks/ShaderPackHooks.java
+++ src/main/java/de/unratedfilms/moviefocus/fmlmod/hooks/ShaderPackHooks.java
@@ -5,7 +5,6 @@ import static de.unratedfilms.moviefocus.shared.Consts.LOGGER;
 import java.io.IOException;
 import java.io.InputStream;
 import de.unratedfilms.moviefocus.fmlmod.shader.ShaderPackPatcher;
-import de.unratedfilms.moviefocus.fmlmod.shader.patchers.SPPs;
 import de.unratedfilms.moviefocus.shared.Consts;
 import shadersmodcore.client.IShaderPack;
 
@@ -15,7 +14,7 @@ public class ShaderPackHooks {
 
         LOGGER.info("Searching for a shader pack patcher that supports the current shader pack");
 
-        for (ShaderPackPatcher patcher : SPPs.PATCHERS) {
+        for (ShaderPackPatcher patcher : ShaderPackPatcher.AVAILABLE_PATCHERS) {
             try {
                 if (patcher.isShaderPackSupported(shaderPack)) {
                     LOGGER.info("Found patcher: '{}'", patcher.getClass().getName());
--- src/main/java/de/unratedfilms/moviefocus/fmlmod/shader/ShaderPackPatcher.java
+++ src/main/java/de/unratedfilms/moviefocus/fmlmod/shader/ShaderPackPatcher.java
@@ -3,9 +3,15 @@ package de.unratedfilms.moviefocus.fmlmod.shader;
 
 import java.io.IOException;
 import java.io.InputStream;
+import com.google.common.collect.ImmutableList;
+import de.unratedfilms.moviefocus.fmlmod.shader.patchers.SPP_OnlyDof;
 import shadersmodcore.client.IShaderPack;
 
 public interface ShaderPackPatcher {
+
+    public static final ImmutableList<ShaderPackPatcher> AVAILABLE_PATCHERS = ImmutableList.<ShaderPackPatcher> builder()
+            .add(new SPP_OnlyDof())
+            .build();
 
     public boolean isShaderPackSupported(IShaderPack shaderPack) throws IOException;
 
