a1dfbbb865920280bc74b20ab9beaf21fbb6ef70
LoadingByte <loadingbyte@quartercode.com>
2016-11-16 18:52:07 +0100

    Add regex methods to MoreStringUtils

--- src/main/java/de/unratedfilms/moviefocus/fmlmod/util/MoreStringUtils.java
+++ src/main/java/de/unratedfilms/moviefocus/fmlmod/util/MoreStringUtils.java
@@ -3,12 +3,28 @@ package de.unratedfilms.moviefocus.fmlmod.util;
 
 import java.util.List;
 import java.util.ListIterator;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MoreStringUtils {
+
+    public static int indexBefore(String str, Pattern searchPattern) {
+
+        Matcher matcher = searchPattern.matcher(str);
+        matcher.find();
+        return matcher.start();
+    }
 
     public static int indexAfter(String str, String searchStr) {
 
         return str.indexOf(searchStr) + searchStr.length();
+    }
+
+    public static int indexAfter(String str, Pattern searchPattern) {
+
+        Matcher matcher = searchPattern.matcher(str);
+        matcher.find();
+        return matcher.end();
     }
 
     public static String insert(String str, int index, String insertedStr) {
@@ -21,9 +37,19 @@ public class MoreStringUtils {
         return insert(str, str.indexOf(searchStr), insertedStr);
     }
 
+    public static String insertBefore(String str, Pattern searchPattern, String insertedStr) {
+
+        return insert(str, indexBefore(str, searchPattern), insertedStr);
+    }
+
     public static String insertAfter(String str, String searchStr, String insertedStr) {
 
         return insert(str, indexAfter(str, searchStr), insertedStr);
+    }
+
+    public static String insertAfter(String str, Pattern searchPattern, String insertedStr) {
+
+        return insert(str, indexAfter(str, searchPattern), insertedStr);
     }
 
     public static List<String> trimAll(List<String> strs) {
